b0f10ae9e327e959717c7ec30b0e6ce46df9ffd2
Spalax-dev <jorgity345@gmail.com>
2024-04-06 13:22:23 -0500

    Correcciones

--- scripts/main.js
+++ scripts/main.js
@@ -1,55 +1,54 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const half = (a, b) => (a + b) / 2;
 
     function findTend(fn, t, maxAttempts = 300) {
         return ft1(fn, t, t - 1, t + 1, maxAttempts);
     }
 
-    function ft1(fn, t, l, r, maxAtm = 300, atm = 1) {
-        const lRes = fn(l);
-        const rRes = fn(r);
+    function ft1(fn, t, left, right, maxAtm = 300, atm = 1) {
+        // Reducir los numeros ingresados
+        const half = (a, b) => (a + b) / 2;
+        // evaluar la funcion por la izquierda y derecha
+        const lRes = fn(left);
+        const rRes = fn(right);
+        // calcular la diferencia entre las funciones evaluadas
         const diff = Math.abs(lRes - rRes);
 
-        if (maxAtm <= atm) {
+        if (maxAtm <= atm) { // si se alcanza la cantidad maxima de intentos, retornar indefinido
             return { attempts: atm, lastDiff: diff, result: undefined };
         }
 
-        if (diff > 0.01) {
-            return ft1(fn, t, half(l, t), half(r, t), maxAtm, atm + 1);
+        if (diff > 0.01) { // si la diferencia es mayor a 0.01, volver a ejecutar
+            return ft1(fn, t, half(left, t), half(right, t), maxAtm, atm + 1);
         }
 
         return {
             attempts: atm,
             lastDiff: diff,
-            result: parseFloat(lRes.toFixed(2)),
+            result: parseFloat(lRes.toFixed(3)),
         };
     }
 
     const tInput = document.querySelector("#t");
-    
+  
     tInput.addEventListener("input", () => {
-      const fnX = (t) => t ** 2;
+      // Se declaran las funciones de cada componente del vector
+      const fnX = (t) => t ** 2; 
       const fnY = (t) => t ** (1 / 2);
+      // Obtener la variable 't' ingresada por el usuario
       let aproximation = parseFloat(tInput.value)
       
       const result = document.querySelector("#result")
+      // Obtener la tendencia de cada componente
       x = findTend(fnX, aproximation)
       y = findTend(fnY, aproximation)
-      
-      if (isNaN(x.result)) {
-        x.result = "No existe";
-      }
 
-      if (isNaN(y.result)) {
-        y.result = "No existe";
+      if (isNaN(x.result) || isNaN(y.result)) { // si el resultado no existe
+        result.innerHTML =`El limite <span class="lim">no existe</span>` 
+      } else {
+        result.innerHTML =`El limite es:  
+          < <span class="lim">${x.result}</span>
+          , 
+          <span class="lim">${y.result}</span> >`
       }
-
-      result.innerHTML =`El limite en X es: <span class="lim">${x.result}</span> <br>El limite en Y es: <span class="lim">${y.result}</span>`
     })
-    
-
-    // console.log('limite en x: ' + findTend(fnX, aproximation));
-    // console.log('limite en y: ' + findTend(fnY, aproximation));
-    // console.log(findTend(fnX, aproximation, 30));
-    // console.log(findTend(fnY, aproximation, 30));
 });
